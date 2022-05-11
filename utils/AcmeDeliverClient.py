838d8d5e69642d8bbb69093ab8b8734b739cbcd8
MoeDev <im@moedev.net>
2022-05-11 17:18:39 +0800

    完成AcmeDeliverClient

--- utils/AcmeDeliverClient.py
+++ utils/AcmeDeliverClient.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# coding:utf-8
 """
 Author  : Moe
 Time    : 2022/4/19 12:20
@@ -15,30 +13,31 @@ import requests
 
 
 class AcmeDeliverClient:
-    def __init__(self, host="https://localhost:9443", domain="", file="", passwd="passwd"):
+    def __init__(self, host="https://localhost:9443", passwd="passwd", proxies=None):
         self.host = host
-        self.domain = domain
-        self.file = file
-        self.timestamp = str(int(time())).encode()
+        self.passwd = passwd
+        self.proxies = proxies
+
+    def getCert(self, domain, file):
+        timestamp = str(int(time())).encode()
         # 构建 checksum
         checksum = hashlib.md5()
         checksum.update(str(random.randint(0, 32767)).encode())
-        checksum.update(self.timestamp)
-        self.checksum = checksum.hexdigest()
+        checksum.update(timestamp)
+        checksum = checksum.hexdigest()
         # 构建 token
         sign = hashlib.md5()
         sign.update(domain.encode())
         sign.update(file.encode())
-        sign.update(passwd.encode())
-        sign.update(self.timestamp)
-        sign.update(self.checksum.encode())
-        self.sign = sign.hexdigest()
-
-    def getCert(self):
-        return requests.get(self.host, params={
-            "domain": self.domain,
-            "file": self.file,
-            "t": self.timestamp,
-            "sign": self.sign,
-            "checksum": self.checksum
-        }).content
+        sign.update(self.passwd.encode())
+        sign.update(timestamp)
+        sign.update(checksum.encode())
+        sign = sign.hexdigest()
+        with requests.get(self.host, params={
+            "domain": domain,
+            "file": file,
+            "t": timestamp,
+            "sign": sign,
+            "checksum": checksum
+        },proxies=self.proxies) as res:
+            return res.content
